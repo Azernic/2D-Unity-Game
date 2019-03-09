fd5976b4a2ac8fd196955a3c89d0756c9701d2d0
Nevin <nevinpuri1901@gmail.com>
2019-03-09 21:28:42 +0100

    i added the files
    literally 2585 changed files what are you DOING

---
+++ First 2D Game/Assets/OnCollision.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollision : MonoBehaviour
+{
+
+    private bool isInCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Player has entered the area");
+        isInCollider = true;
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Player has exited the area");
+        isInCollider = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && isInCollider == true)
+        {
+            Debug.Log("ok");
+        }
+    }
+}
